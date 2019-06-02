a8fd0319c9f76f320f7656c032b362bbdcc785b3
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-06-02 19:54:23 +0800

    initial commit

---
+++ jsonapp/jsonapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonapp
+//
+//  Created by Stephenson Ang on 02/06/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
